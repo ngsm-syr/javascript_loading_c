9e4426461dc0ddeac936648eaed907aec0eb0c96
Nagashima Sayuri <106061832+ngsm-syr@users.noreply.github.com>
2023-10-05 10:33:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,3 @@ function animation(){
   loading.classList.add('loaded');
 }
 window.addEventListener('load', animation);
-
-// アロー関数を使った書き方
-// window.addEventListener('load', () => {
-//   loading.classList.add('loaded');
-// });
